0db338504fdd72d1a75c1b7567349b878af80a7e
Eden <xuda27@163.com>
2018-08-27 19:33:31 +0800

    Update ExecutorMain.java

--- src/main/java/com/xudaweb/javaconcurrency/java5/ExecutorMain.java
+++ src/main/java/com/xudaweb/javaconcurrency/java5/ExecutorMain.java
@@ -11,32 +11,30 @@ import java.util.concurrent.Executors;
  * @date 2017年10月27日
  */
 public class ExecutorMain {
-	
 	public static void main(String[] args) {
 		//执行器，线程池（ThreadPoolExecutor）是它的一种实现
 		//线程池是线程复用
 		
 		//固定长度线程池
-        //好处：线程数量固定，不会存在线程重复初始化
-        //坏处：没有对队列大小进行限制，线程初始化后，再也不能回收线程资源
+        	//好处：线程数量固定，不会存在线程重复初始化
+        	//坏处：没有对队列大小进行限制，线程初始化后，再也不能回收线程资源
 		Executor executor = Executors.newFixedThreadPool(1);
 		
 		//CachedThreadPool 执行线程不固定，
 		//好处：可以把新增任务全部缓存在一起，
-	    //坏处：只能用在短时间完成的任务（占用时间较长的操作可以导致线程数无限增大，系统资源耗尽）
-//		Executor executor = Executors.newCachedThreadPool();
+	    	//坏处：只能用在短时间完成的任务（占用时间较长的操作可以导致线程数无限增大，系统资源耗尽）
+		//Executor executor = Executors.newCachedThreadPool();
 		
 		//单线程线程池
-	    //好处：针对单cpu，单线程避免系统资源的抢夺
-	    //坏处：多cpu多线程时，不能完全利用cpu资源
-//		Executor executor = Executors.newSingleThreadExecutor();
+	    	//好处：针对单cpu，单线程避免系统资源的抢夺
+	    	//坏处：多cpu多线程时，不能完全利用cpu资源
+		//Executor executor = Executors.newSingleThreadExecutor();
 		executor.execute(new Runnable() {
 			@Override
 			public void run() {
 				System.out.printf("[Thread：%s]Hello,World...\n", Thread.currentThread().getName());
 			}
 		});
-		
 		//合理地关闭线程池是非常重要的
 		if (executor instanceof ExecutorService) {
 			ExecutorService executorService = ExecutorService.class.cast(executor);
@@ -50,7 +48,5 @@ public class ExecutorMain {
 				}
 			}
 		}
-
-		//Java 5 开始实施 AutoCloseable,I/O,JDBC
 	}
 }
